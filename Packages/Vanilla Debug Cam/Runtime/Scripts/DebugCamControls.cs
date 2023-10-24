d52c821756e385cabaaf3230faf7e4f4061be669
Lucas <lucas@vanilla.plus>
2023-10-25 01:38:14 +1100

    Debug Cam 2.0.5 - Activate anywhere with F5!

--- Packages/Vanilla Debug Cam/Runtime/Scripts/DebugCamControls.cs
+++ Packages/Vanilla Debug Cam/Runtime/Scripts/DebugCamControls.cs
@@ -1,13 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 using UnityEngine;
 
 namespace Vanilla.DebugCam
 {
+    
+    [Serializable]
     public class DebugCamControls : MonoBehaviour
     {
 
+        public static GameObject debugCam;
+
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void Reset()
+        {
+            #if UNITY_EDITOR
+            if (debugCam != null) Destroy(debugCam);
+
+            debugCam = null;
+            #endif
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static async Task Init()
+        {
+            #if UNITY_EDITOR
+            while (debugCam == null)
+            {
+                if (Input.GetKeyDown(KeyCode.F5))
+                {
+                    var path = Path.Combine("Packages",
+                                            "vanilla.debug-cam",
+                                            "Runtime",
+                                            "Prefabs",
+                                            "Debug Cam.prefab");
+                    
+                    var asset = (GameObject) AssetDatabase.LoadAssetAtPath(assetPath: path,
+                                                                           type: typeof(GameObject));
+
+                    if (asset == null)
+                    {
+                        Debug.LogWarning(message: $"We were unable to find the chosen prefab. It was expected to be found at the following path:\n\n{path}");
+
+                        return;
+                    }
+
+                    debugCam = PrefabUtility.InstantiatePrefab(assetComponentOrGameObject: asset) as GameObject;
+                    
+                    break;
+                }
+
+                await Task.Yield();
+            }
+            #endif
+        }
+        
         private Transform _t;
         private Camera _cam;
         
@@ -31,16 +85,19 @@ namespace Vanilla.DebugCam
         public KeyCode inputLock = KeyCode.Mouse1;
         
         [Header(header: "Look Input")]
-        public float xSensitivity = 2.0f;
-        public float ySensitivity = 2.0f;
+        public float xSensitivity = 4.0f;
+        public float ySensitivity = 4.0f;
+        public float zSensitivity = 4.0f;
         
         [Header(header: "Move Input")]
-        public KeyCode toggle = KeyCode.F1;
+        public KeyCode toggle = KeyCode.F5;
 
         public KeyCode forward = KeyCode.W;
-        public KeyCode back = KeyCode.S;
-        public KeyCode left = KeyCode.A;
-        public KeyCode right = KeyCode.D;
+        public KeyCode back    = KeyCode.S;
+        public KeyCode left    = KeyCode.A;
+        public KeyCode right   = KeyCode.D;
+        public KeyCode up   = KeyCode.E;
+        public KeyCode down   = KeyCode.Q;
 
         public KeyCode boost = KeyCode.LeftShift;
 
@@ -53,7 +110,7 @@ namespace Vanilla.DebugCam
         private Vector3    eulerTarget;
         private Quaternion rotationVelocity;
 
-        private MeshRenderer[] meshes = new MeshRenderer[0];
+        private MeshRenderer[] meshes = Array.Empty<MeshRenderer>();
 
         public bool movementSmoothing = true;
         
@@ -70,9 +127,13 @@ namespace Vanilla.DebugCam
 
         void Awake()
         {
-            #if !DEBUG
+            #if !UNITY_EDITOR
             Destroy(gameObject);
+            
+            return;
             #endif
+
+            if (debugCam == null) debugCam = gameObject;
 
             meshes = GetComponentsInChildren<MeshRenderer>();
         }
@@ -86,19 +147,13 @@ namespace Vanilla.DebugCam
             _cam.enabled = _active;
             _cam.depth   = _active ? 100.0f : -100.0f;
                 
-            foreach (var m in meshes)
-            {
-                m.enabled = _active;
-            }
+            foreach (var m in meshes) m.enabled = _active;
         }
 
         void Update()
         {
             #if UNITY_EDITOR
-            if (Input.GetKeyDown(key: toggle))
-            {
-                active = !_active;
-            }
+            if (Input.GetKeyDown(key: toggle)) active = !_active;
 
             if (!active) return;
 
@@ -119,20 +174,24 @@ namespace Vanilla.DebugCam
                     if (Input.GetKey(key: back)) positionTarget    -= _t.forward * s;
                     if (Input.GetKey(key: left)) positionTarget    -= _t.right   * s;
                     if (Input.GetKey(key: right)) positionTarget   += _t.right   * s;
+                    if (Input.GetKey(key: up)) positionTarget      += _t.up      * s;
+                    if (Input.GetKey(key: down)) positionTarget    -= _t.up      * s;
 
                     // FOV Zoom
 
-                    _cam.fieldOfView -= Input.GetAxis(axisName: "Mouse ScrollWheel") * 10.0f;
+                    _cam.fieldOfView -= Input.GetAxis(axisName: "Mouse ScrollWheel") * zSensitivity;
                 }
 
                 _t.position = Vector3.SmoothDamp(current: transform.position,
                                                  target: positionTarget,
                                                  currentVelocity: ref positionVelocity,
-                                                 smoothTime: 0.075f);
+                                                 smoothTime: 0.075f,
+                                                 maxSpeed: 100.0f, 
+                                                 deltaTime: Time.unscaledDeltaTime);
 
-                _t.rotation = Quaternion.Lerp(a: transform.rotation,
-                                                   b: Quaternion.Euler(euler: eulerTarget),
-                                                   t: Time.deltaTime * 20.0f);
+                _t.rotation = Quaternion.Slerp(a: transform.rotation,
+                                               b: Quaternion.Euler(euler: eulerTarget),
+                                               t: Time.unscaledDeltaTime * 20.0f);
             }
             else
             {
